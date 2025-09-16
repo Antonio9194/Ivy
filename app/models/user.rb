94d9123cff1593e7a1d1b115c1e87234f2926554
Antonio Vinciguerra <anto.vinciguerra@hotmail.com>
2025-09-16 14:59:24 +0800

    done

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :chats, dependent: :destroy
+  has_many :records, dependent: :destroy
   has_many :attachments, through: :chats
 
   validates :name, presence: true
